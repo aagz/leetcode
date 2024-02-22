ab007f58339fa91eead7bb846975c430709a41e7
Vladimir Chubar <46520039+aagz@users.noreply.github.com>
2024-02-23 02:59:05 +0300

    Time: 56 ms (60.91%) | Memory: 52.2 MB (22.12%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function search(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = (high + low) / 2 + 0.5 | 0;
+
+    if (nums[mid] < target) {
+      low = mid + 1;
+    } else if (nums[mid] > target) {
+      high = mid - 1;
+    } else if (nums[mid] == target) {
+      return mid;
+    }
+  }
+
+  return -1;
+}
