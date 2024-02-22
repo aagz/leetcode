5d17a56fc5aeb35c34ba1597db86526e086f02f0
Vladimir Chubar <46520039+aagz@users.noreply.github.com>
2024-02-23 02:56:49 +0300

    Time: 51 ms (68.37%) | Memory: 49.2 MB (21.62%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+  let mid;
+
+  while (low <= high) {
+    mid = (high + low) / 2 + 0.5 | 0;
+
+    if (nums[mid] < target) {
+      low = mid + 1;
+    } else if (nums[mid] > target) {
+      high = mid - 1;
+    } else if (nums[mid] == target) {
+      return mid;
+    }
+  }
+
+  return nums[mid] < target ? mid + 1 : mid;
+};
